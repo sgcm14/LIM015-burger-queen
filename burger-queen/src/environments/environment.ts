325189b0195d3f37d32d6ee0e7c8f7316b888c59
BitzyG <gv.itzelandrea@gmail.com>
2021-09-30 08:59:36 -0500

    Configuration

---
+++ burger-queen/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+  apiKey: "AIzaSyDGMGO4L3eG3APw09zkNBjetEEfVSePYpo",
+  authDomain: "burger-queen-ffe43.firebaseapp.com",
+  projectId: "burger-queen-ffe43",
+  storageBucket: "burger-queen-ffe43.appspot.com",
+  messagingSenderId: "109474495828",
+  appId: "1:109474495828:web:3856386618300c16eb9cc1"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
