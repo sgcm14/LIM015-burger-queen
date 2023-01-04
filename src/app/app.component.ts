29b3a86bdd96c0fb5b2545d9a5eef52c766ff73a
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2023-01-04 11:26:44 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
