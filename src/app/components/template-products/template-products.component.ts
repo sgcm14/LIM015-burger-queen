1c78d985d0cca343b05d8b81e5ee3a67428d57f4
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2023-01-04 11:32:18 -0500

    Update template-products.component.ts

--- src/app/components/template-products/template-products.component.ts
+++ src/app/components/template-products/template-products.component.ts
@@ -7,24 +7,24 @@ import { CartService } from '../../services/cart.service';
   styleUrls: ['./template-products.component.css']
 })
 export class TemplateProductsComponent implements OnInit {
-  @Input () product: any;
+  @Input() product: any;
   itemsCart = this.cartService.getItems();
 
   constructor(private cartService: CartService) { }
 
-  ngOnInit(): void {    }
+  ngOnInit(): void { }
 
   addToCart(product: any) {
-      // agrega producto al carrito si está vacio
-    if(this.itemsCart.length === 0) {
+    // agrega producto al carrito si está vacio
+    if (this.itemsCart.length === 0) {
       this.cartService.addToCart(product);
     } else {
-        // agrega el producto si el id es diferente a los agregados
-      if(!this.itemsCart.find( (item: any) => item.id === product.id)) {
+      // agrega el producto si el id es diferente a los agregados
+      if (!this.itemsCart.find((item: any) => item.id === product.id)) {
         this.cartService.addToCart(product);
         // si encuentra al id actualiza su cantidad
       } else {
-        this.cartService.updateCart(product,'+');
+        this.cartService.updateCart(product, '+');
       }
     }
   }
