688e26e58454280ae1d856453f96a4cb71eb142f
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2023-01-04 11:25:04 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  firebase : {
-  apiKey: "AIzaSyDGMGO4L3eG3APw09zkNBjetEEfVSePYpo",
-  authDomain: "burger-queen-ffe43.firebaseapp.com",
-  projectId: "burger-queen-ffe43",
-  storageBucket: "burger-queen-ffe43.appspot.com",
-  messagingSenderId: "109474495828",
-  appId: "1:109474495828:web:3856386618300c16eb9cc1"
+  firebase: {
+    apiKey: "AIzaSyDGMGO4L3eG3APw09zkNBjetEEfVSePYpo",
+    authDomain: "burger-queen-ffe43.firebaseapp.com",
+    projectId: "burger-queen-ffe43",
+    storageBucket: "burger-queen-ffe43.appspot.com",
+    messagingSenderId: "109474495828",
+    appId: "1:109474495828:web:3856386618300c16eb9cc1"
   }
 };
